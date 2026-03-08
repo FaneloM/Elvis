26819d5eb933ca063c946b2d3d6c3078ad6db569
Fanelo Mhlambi <fmhlambi@icloud.com>
2026-03-08 16:43:24 +0200

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Fanelo Mhlambi on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
