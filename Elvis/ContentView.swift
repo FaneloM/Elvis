3d24d164ad382e2f60032ddf95766eeb20941d82
Fanelo Mhlambi <fmhlambi@icloud.com>
2026-03-08 17:24:24 +0200

    This is my Elvis Project

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message: String = ""
+    @State private var shownImage: Image = Image(systemName: "")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'But")
+                .font(.largeTitle)
+                .foregroundStyle(Color(.purple))
+                .bold(true)
+            
+            Spacer()
+            
+            shownImage
+                .font(Font.system(size: 300))
+                
+            
+            Text(message)
+                .font(.largeTitle)
+                .foregroundStyle(Color(.purple))
+                .bold(true)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    shownImage = Image(systemName: "peacesign")
+                }
+                
+                Button("Love") {
+                    message = "Love"
+                    shownImage = Image(systemName: "heart")
+                }
+                
+                Button("Understanding") {
+                    message = "Understanding"
+                    shownImage = Image(systemName: "lightbulb")
+                }
+            }
+            .buttonStyle(.glassProminent)
+            .tint(Color(.purple))
+            
+            
         }
         .padding()
     }
